61eeca9aaf3021b736f723c9febf1f91498702ec
Antony.Glim <Antony.Glim@gmail.com>
2019-01-14 22:57:27 +0300

    Initial commit

---
+++ android/src/antonyglims/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package antonyglims.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import antonyglims.game.LibGDXsimpleGameExample;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LibGDXsimpleGameExample(), config);
+	}
+}
