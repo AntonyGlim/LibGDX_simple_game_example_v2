37744c114a3517cd40acaf17ee0ce221ee301ba5
Antony.Glim <Antony.Glim@gmail.com>
2019-01-15 20:54:58 +0300

    Настройка Starter классов Done!

--- desktop/src/antonyglim/gmail/desktop/DesktopLauncher.java
+++ desktop/src/antonyglim/gmail/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import antonyglim.gmail.SimpleGameExample;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "GlimSimpleGame";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new SimpleGameExample(), config);
 	}
 }
