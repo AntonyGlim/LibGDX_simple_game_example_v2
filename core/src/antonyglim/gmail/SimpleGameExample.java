f49790211ceccb0b865709c0fdb292a95c97a554
Antony.Glim <Antony.Glim@gmail.com>
2019-01-15 21:13:29 +0300

    Загрузка Assets Done!

--- core/src/antonyglim/gmail/SimpleGameExample.java
+++ core/src/antonyglim/gmail/SimpleGameExample.java
@@ -2,32 +2,52 @@ package antonyglim.gmail;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SimpleGameExample extends ApplicationAdapter {
+
+	//загрузить assets и сохранить ссылки на них
+	Texture dropImage;
+	Texture bucketImage;
+	Sound dropSonud;
+	Music rainMusic;
+
 	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Загрузка изображений капли и ведра, каждое размером 64x64 пикселей
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// Загрузка звукового эффекта падающей капли и фоновой "музыки" дождя
+		dropSonud = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// Сразу же воспроизводиться музыка для фона
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+//		batch.dispose();
+//		img.dispose();
 	}
 }
