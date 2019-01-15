9d0e797c88db63f728fd35c445d0e6fe102a4c77
Antony.Glim <Antony.Glim@gmail.com>
2019-01-15 21:31:47 +0300

    Камера и SpriteBatch Done!

--- core/src/antonyglim/gmail/SimpleGameExample.java
+++ core/src/antonyglim/gmail/SimpleGameExample.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -16,6 +17,8 @@ public class SimpleGameExample extends ApplicationAdapter {
 	Sound dropSonud;
 	Music rainMusic;
 
+	//добавим два новых поля в класс и назовем их camera и batch
+	OrthographicCamera camera;
 	SpriteBatch batch;
 
 	@Override
@@ -32,22 +35,24 @@ public class SimpleGameExample extends ApplicationAdapter {
 		rainMusic.setLooping(true);
 		rainMusic.play();
 
-//		batch = new SpriteBatch();
-//		img = new Texture("badlogic.jpg");
+		//создаем камеру
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);		//камера всегда показывает область мира игры
+
+		//создать SpriteBatch
+		batch = new SpriteBatch();
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-//		batch.draw(img, 0, 0);
-//		batch.end();
+
+
 	}
 	
 	@Override
 	public void dispose () {
-//		batch.dispose();
-//		img.dispose();
 	}
 }
